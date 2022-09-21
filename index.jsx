b7a90a973ba2de65e292c5368847160b7aecafa4
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-09-21 14:44:01 -0300

    02:05 - Criando componente React

--- index.jsx
+++ index.jsx
@@ -2,8 +2,24 @@ const { Typography } = MaterialUI;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function MyParagrafo() {
-  return <Typography variant="h6">Olá mundo novamente</Typography>;
+function MyButton(props) {
+  return (
+    <button {...props} className={props.variant}>
+      {props.children}
+    </button>
+  );
 }
 
-root.render(<MyParagrafo />);
+root.render(
+  <React.Fragment>
+    <MyButton style={{ margin: "8px" }} variant="Mycontained">
+      click aqui !
+    </MyButton>
+    <MyButton style={{ margin: "8px" }} variant="Myoutlined">
+      click aqui !
+    </MyButton>
+    <MyButton style={{ margin: "8px" }} variant="MytextButton">
+      click aqui !
+    </MyButton>
+  </React.Fragment>
+);
