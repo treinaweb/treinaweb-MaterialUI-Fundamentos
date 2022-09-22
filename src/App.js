5baf78ee22b7b262be27cf5620a7a30754bdd335
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-09-22 14:28:30 -0300

    03:13 - MediaQuery

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Box from '@mui/material/Box'
 import { Header } from './ui/surfaces/header/header';
 import { Typography } from '@mui/material';
-
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery';
 function App() {
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('sm'))
   return (
     <Box >
       <Header />
       <main>
         <Box sx={{ pt: 15, maxWidth: 'sm', mx: 'auto' }} component={'section'} >
-          <Typography component={'h1'} variant='h4' align='center'>Album de fotos</Typography>
-          <Typography paragraph variant='body2' align='center'
+          <Typography component={'h1'} variant={matches ? 'h4' : 'h2'} align='center'>Album de fotos</Typography>
+          <Typography paragraph variant={matches ? 'body2' : 'subtitle1'} align='center'
             sx={{ mt: 2 }}
           >
             Dolor aliqua commodo enim eiusmod. Ut laboris
