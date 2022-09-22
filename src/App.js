f0e185cb9b2713c4e0197b37c00b4eac4d978395
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-09-22 12:49:52 -0300

    03:11 - Criando Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Box, Typography, Link } from '@mui/material'
+import Box from '@mui/material/Box'
+import { Header } from './ui/surfaces/header/header';
 
 function App() {
   return (
-    <Box className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Typography variant='h6' paragraph>
-          Edit <code>src/App.js</code> and save to reload.
-        </Typography>
-        <Link
-
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </Link>
-      </header>
+    <Box >
+      <Header />
     </Box>
   );
 }
