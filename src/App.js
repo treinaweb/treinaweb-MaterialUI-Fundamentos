bbcf34491121a2b24b2d86f5aa510a180306dbfc
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-09-22 13:49:55 -0300

    03:12 - Criando Section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@
 import './App.css';
 import Box from '@mui/material/Box'
 import { Header } from './ui/surfaces/header/header';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <Box >
       <Header />
+      <main>
+        <Box sx={{ pt: 15, maxWidth: 'sm', mx: 'auto' }} component={'section'} >
+          <Typography component={'h1'} variant='h4' align='center'>Album de fotos</Typography>
+          <Typography paragraph variant='body2' align='center'
+            sx={{ mt: 2 }}
+          >
+            Dolor aliqua commodo enim eiusmod. Ut laboris
+            reprehenderit aliquip velit nulla magna. Eiusmod
+            duis et sunt veniam. Sit eu sint incididunt ea laboris
+            laborum mollit quis. Nisi dolore magna sunt enim nisi
+            reprehenderit nisi.
+          </Typography>
+        </Box>
+      </main>
     </Box>
   );
 }
